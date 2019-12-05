6e15280a1f8e5815f7eb69ee1654a6f8233705df
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 03:38:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,13 +15,13 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet", age: "50"},
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
         name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
